3dc490af3ac4b6db69e7742552c028c370088b74
Vicente Javier Viera Guizar <vicentejvg@outlook.com>
2026-06-07 19:31:59 -0600

    Bump project version to 0.1.1, add report module and update CMake configurations

--- app/main.cpp
+++ app/main.cpp
@@ -2,13 +2,16 @@
 #include <string>
 #include <config.hpp>
 #include <filereader.hpp>
+#include <report.hpp>
 
 int main(){
     std::cout<<project_name<<std::endl;
     std::cout<<project_version<<std::endl;
 
-    const std::string text = FileReader::readFile("/home/vicente/Source/Text-Analysis/data/war_and_peace.txt");
-    std::cout<<text.substr(0, 14);
+    // const std::string text = FileReader::readFile("/home/vicente/Source/Text-Analysis/data/war_and_peace.txt");
+    // std::cout<<text.substr(0, 14);
+
+    std::cout<<uwu<<std::endl;
 
     return 0;
 }
