aac3a309d4f74e3efbdaeba2d0b61e239481ffcd
Vicente Javier Viera Guizar <vicentejvg@outlook.com>
2026-06-10 11:40:01 -0600

    Remove cef submodule, update CMake configurations, and enhance main output to include C++ standard

--- app/main.cpp
+++ app/main.cpp
@@ -7,6 +7,7 @@
 int main(){
     std::cout<<project_name<<std::endl;
     std::cout<<project_version<<std::endl;
+    std::cout<<"C++ Standard: "<<__cplusplus<<std::endl;
 
     // const std::string text = FileReader::readFile("/home/vicente/Source/Text-Analysis/data/war_and_peace.txt");
     // std::cout<<text.substr(0, 14);
